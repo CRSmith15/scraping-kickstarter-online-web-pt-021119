6a4868f480e85ed042227198bee1076624e1fecd
Chance Smith <chancesmith97@hotmail.com>
2019-04-20 04:15:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,6 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = {}
   kickstarter.css("li.project.grid_4").each do|project|
-    
     title = project.css("h2.bbcard_name strong a").textprojects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
